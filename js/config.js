57fac7ed0108e89d6263a934e2d7cb9dce191a4e
malsllami <44532530+malsllami@users.noreply.github.com>
2026-01-21 23:46:23 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // ضع رابط Web App بعد النشر
 // مثال: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
 window.APP_CONFIG = {
-  apiBase: "https://script.google.com/macros/s/AKfycbzCXIvF7bEUnGDlzxvfG68v1FvjNoZhdfgKN2utsmIKVUxDCmLME2woLYchXKN84HJm/exec"
+  apiBase: "https://script.google.com/macros/s/AKfycbyotYOh9EsQCQfb6gysoJsLDv6DQYLEGcZ-FUbFDQ54nVwnaLCDW_sA_OBEQQMA78Xm/exec"
 };
